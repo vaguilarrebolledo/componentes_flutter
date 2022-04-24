baec6fce59e142b9413891119e19f04c27ad3d9f
Victor Aguilar Rebolledo <vaguilar@Victors-Mac-mini.local>
2022-04-24 17:29:54 -0400

    componentes flutter

---
+++ lib/screens/listview1_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class Listview1Screen extends StatelessWidget {
+  final options = const ['Megaman' , 'Metal Gear', 'Super Smash', 'Final Fantasy'];
+
+  const Listview1Screen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('ListView Tipo 1'),
+        ),
+        body: ListView(
+          children:  [
+            ...options.map(
+              (game) =>  ListTile(
+                title: Text(game),
+                trailing: Icon(Icons.arrow_forward_ios),
+              )
+              ).toList(),
+              Divider(),
+              
+
+
+          // ListTile(
+          //   leading: Icon(Icons.ac_unit),
+          //   title: Text('Titulo'),
+          // ),
+        ]));
+  }
+}
