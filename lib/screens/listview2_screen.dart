fb8824b6b5b4272db7b4825a5e59310b2e5ad066
Victor Aguilar Rebolledo <vaguilar@Victors-Mac-mini.local>
2022-04-24 21:10:31 -0400

    ordenando routes y agregando initialRoute

--- lib/screens/listview2_screen.dart
+++ lib/screens/listview2_screen.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 
 class Listview2Screen extends StatelessWidget {
-  final options = const ['Megaman' , 'Metal Gear', 'Super Smash', 'Final Fantasy'];
+  final options = const [
+    'Megaman',
+    'Metal Gear',
+    'Super Smash',
+    'Final Fantasy'
+  ];
 
   const Listview2Screen({Key? key}) : super(key: key);
 
@@ -10,16 +15,20 @@ class Listview2Screen extends StatelessWidget {
     return Scaffold(
         appBar: AppBar(
           title: const Text('ListView Tipo 2'),
+          elevation: 5,
+          backgroundColor: Colors.indigo,
         ),
         body: ListView.separated(
-            itemCount: options.length,//numero de items (arreglo)
-            itemBuilder: (context,i) => ListTile(
-                title: Text(options[i]),
-                trailing: const Icon(Icons.arrow_forward_ios),
-              ), 
-            separatorBuilder: (_,__) => const Divider(), 
-          )
-        
-        );
+          itemCount: options.length, //numero de items (arreglo)
+          itemBuilder: (context, i) => ListTile(
+            title: Text(options[i]),
+            trailing: const Icon(Icons.arrow_forward_ios),
+            onTap: () {
+              final game = options[i];
+              print(game);
+            },
+          ),
+          separatorBuilder: (_, __) => const Divider(),
+        ));
   }
 }
